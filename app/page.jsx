e0f66b7127c021fb470a77ebef17182674768e0c
TereseBo <terese.boderus@gmail.com>
2024-11-18 18:59:19 +0100

    Added image to home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi"
 import { Socials } from "@/components/Socials";
+import { Photo } from "@/components/Photo";
 
 export default function Home() {
   return (
@@ -8,11 +9,11 @@ export default function Home() {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* Presentation text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Fullstack developer</span>
             <h1 className="h1 mb-6">Hello I'm
               <br />
-              <span className="text-accent">Terese</span>
+              <span className="text-accent">Terese Bodérus</span>
             </h1>
             <p className="max-w[500px] mb-9 text-white/80"> I have a passion for biology, technology and problem-solving.
               I'm capable in various programming languages and technologies.
@@ -23,7 +24,7 @@ export default function Home() {
                 <span>Download CV</span>
                 <FiDownload className="text-large" />
               </Button>
-              <div classname="mb-8 xl:mb-0">
+              <div className="mb-8 xl:mb-0">
                 <Socials
                   containerStyles="flex gap-6"
                   iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center
@@ -32,7 +33,10 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div>photo</div>
+          {/* photo */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo/>
+          </div>
         </div>
       </div>
     </section>
