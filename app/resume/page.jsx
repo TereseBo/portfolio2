5d3b4cd53959f92ad0cf3ce401dcd5bc56f767c6
TereseBo <terese.boderus@gmail.com>
2024-11-21 19:06:37 +0100

    Updated tabs for resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,9 +1,9 @@
 
 "use client"
-import { FaHtml5, FaCss3, FaJS, FaReact, FaNodeJs } from "react-icons/fa"
-import { SiTailwindcss, SiNextgdotjs } from "react-icons/si"
-import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from " @components/ui/tooltip"
+import { FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs, FaFlask, FaPython } from "react-icons/fa"
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { motion } from "framer-motion"
 
@@ -11,7 +11,8 @@ import { motion } from "framer-motion"
 //TODO: Update experience data 
 //TODO: Update educational data 
 //TODO: Update skill data 
-cons
+const loremIpsum = "Lorem ipsum odor amet, consectetuer adipiscing elit. Ornare cursus dui nullam pretium, mauris tellus parturient? Rhoncus suscipit platea suscipit eleifend turpis facilisis mollis. Fringilla urna varius nisi amet litora tellus at interdum nisi. Sed sodales turpis cras purus tellus consequat porttitor. Nascetur a condimentum ex parturient rhoncus lobortis enim."
+
 //Personal data
 const about = {
     title: "About me",
@@ -49,121 +50,154 @@ const about = {
 }
 
 //Experience data
-const experience={
-    icon:"assets/resume/badge.svg",
+const experience = {
+    icon: "assets/resume/badge.svg",
     title: "My experience",
     description: loremIpsum,
-    items:[
+    items: [
         {
-            company:"Clinical Genomics, SciLifelabs",
-            position:"Fullstack developer intern",
-            duration:"Oct 2023 - Apr 2024"
+            company: "Clinical Genomics, SciLifelabs",
+            position: "Fullstack developer intern",
+            duration: "Oct 2023 - Apr 2024"
         },
         {
-            company:"Centre for rare diseases",
-            position:"Information Scientist",
-            duration:"Sept 2020 - Present"
+            company: "Centre for rare diseases",
+            position: "Information Scientist",
+            duration: "Sept 2020 - Present"
         },
         {
-            company:"Department of Clinical Genetics",
-            position:"Laboratory technichian",
-            duration:"Jun 2014 - Aug 2022"
+            company: "Department of Clinical Genetics",
+            position: "Laboratory technichian",
+            duration: "Jun 2014 - Aug 2022"
         },
         {
-            company:"Department of Clinical Genetics",
-            position:"Education coordinator",
-            duration:"Apr 2021 - Feb 2022"
+            company: "Department of Clinical Genetics",
+            position: "Education coordinator",
+            duration: "Apr 2021 - Feb 2022"
         },
         {
-            company:"Department of Clinical Genetics",
-            position:"Student coordinator",
-            duration:"Aug 2018 - Dec 2021"
+            company: "Department of Clinical Genetics",
+            position: "Student coordinator",
+            duration: "Aug 2018 - Dec 2021"
         },
     ]
 }
 
 //Education data
 
-const education={
-    icon:"assets/resume/cap.svg",
+const education = {
+    icon: "assets/resume/cap.svg",
     title: "My experience",
     description: loremIpsum,
-    items:[
+    items: [
         {
-            institution:"Nackademin AB",
-            degree:"Full stack web developer, open source",
-            duration:"Sep 2022 - May 2024"
+            institution: "Nackademin AB",
+            degree: "Full stack web developer, open source",
+            duration: "Sep 2022 - May 2024"
         },
         {
-            institution:"Karolinska Institutet",
-            degree:"Bachelor of Medical Sciences in laboratory science",
-            duration:"Sep 2011 - Jun 2014"
+            institution: "Karolinska Institutet",
+            degree: "Bachelor of Medical Sciences in laboratory science",
+            duration: "Sep 2011 - Jun 2014"
         },
         {
-            institution:"Nackademin AB",
-            degree:"Full stack web developer, open source",
-            duration:"Sep 2022 - May 2024"
+            institution: "Nackademin AB",
+            degree: "Full stack web developer, open source",
+            duration: "Sep 2022 - May 2024"
         },
         {
-            institution:"Nackademin AB",
-            degree:"Full stack web developer, open source",
-            duration:"Sep 2022 - May 2024"
+            institution: "Nackademin AB",
+            degree: "Full stack web developer, open source",
+            duration: "Sep 2022 - May 2024"
         },
         {
-            institution:"Nackademin AB",
-            degree:"Full stack web developer, open source",
-            duration:"Sep 2022 - May 2024"
+            institution: "Nackademin AB",
+            degree: "Full stack web developer, open source",
+            duration: "Sep 2022 - May 2024"
         },
         {
-            institution:"Nackademin AB",
-            degree:"Full stack web developer, open source",
-            duration:"Sep 2022 - May 2024"
+            institution: "Nackademin AB",
+            degree: "Full stack web developer, open source",
+            duration: "Sep 2022 - May 2024"
         },
         {
-            institution:"Nackademin AB",
-            degree:"Full stack web developer, open source",
-            duration:"Sep 2022 - May 2024"
+            institution: "Nackademin AB",
+            degree: "Full stack web developer, open source",
+            duration: "Sep 2022 - May 2024"
         },
     ]
 }
 
 //Skill data
-const skills={
-    title:"My skills",
-    description:loremIpsum,
-    skillList:[
+const skills = {
+    title: "My skills",
+    description: loremIpsum,
+    skillList: [
         {
-            icon:<FaHtml5/>,
-            name:"html 5"
+            icon: <FaHtml5 />,
+            name: "html 5"
         },
         {
-            icon:<FaCss3/>,
-            name:"css 3"
+            icon: <FaCss3 />,
+            name: "css 3"
         },
         {
-            icon:<FaJS/>,
-            name:"javascript"
+            icon: <FaJs />,
+            name: "javascript"
         },
         {
-            icon:<FaNodeJs/>,
-            name:"node.js"
+            icon: <FaNodeJs />,
+            name: "node.js"
         },
         {
-            icon:<FaReact/>,
-            name:"React.js"
+            icon: <FaReact />,
+            name: "React.js"
         },
         {
-            icon:<SiNextgdotjs/>,
-            name:"Next.js"
+            icon: <SiNextdotjs />,
+            name: "Next.js"
         },
         {
-            icon:<SiTailwindcss/>,
-            name:"tailwind.css"
+            icon: <SiTailwindcss />,
+            name: "tailwind.css"
         },
     ]
 }
 export default function Resume() {
     return (
-        <motion.div>Resume page</motion.div>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{
+                opacity: 1,
+                transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }
+            }}
+            className="min-h-[80vw] flex items-center justify-center py-12 xl:py-2"
+        >
+            <div className="container mx-auto">
+                <Tabs
+                    defaultValue="experience"
+                    className="flex flex-col xl:flex-row gap-[60px]"
+                >
+                    <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+                        <TabsTrigger value="experience">Experience</TabsTrigger>
+                        <TabsTrigger value="education">Education</TabsTrigger>
+                        <TabsTrigger value="skills">Skills</TabsTrigger>
+                        <TabsTrigger value="about">About me</TabsTrigger>
+                    </TabsList>
+
+                    {/* Contents */}
+                    <div className="min-h-[70vh] w-full">
+                        {/* experience */}
+                        <TabsContent value="experience" className="w-full ">experience</TabsContent>
+                        {/* education */}
+                        <TabsContent value="experience" className="w-full ">education</TabsContent>
+                        {/* skills */}
+                        <TabsContent value="experience" className="w-full ">skills</TabsContent>
+                        {/* about me */}
+                        <TabsContent value="experience" className="w-full ">about me</TabsContent>
+                    </div>
+                </Tabs>
+            </div>
+        </motion.div>
     )
 }
