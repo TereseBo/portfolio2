d28b9deabe4dc03d9513e2cda62090504d1774fd
TereseBo <terese.boderus@gmail.com>
2024-11-25 17:48:05 +0100

    Updated contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,87 @@
-export default function Contact(){
-    return(
-        <div>Contact page</div>
+
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa"
+import { motion } from "framer-motion"
+
+const info = [
+    {
+        icon: <FaPhoneAlt />,
+        title: "Phone",
+        description: "nr"
+    },
+    {
+        icon: <FaEnvelope />,
+        title: "Email",
+        description: "mail"
+    },
+    {
+        icon: <FaMapMarkerAlt />,
+        title: "Address",
+        description: "Stockholm"
+    },
+]
+
+export default function Contact() {
+    return (
+        <motion.section initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }, }}
+            className="py-6">
+            <div className="container mx-auto">
+                <div className="flex flex-col xl:flex-row gap-[30px]">
+                    {/* Contact form  */}
+                    <div className="xl:w-[54%] order-2 xl:order-none">
+                        <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+                            <h3 className="text-4xl text-accent ">Let's work together!</h3>
+                            <p className="text-white/60">Placeholder text for something catchy</p>
+                            <div className=" grid grid-cols-1 md:grid-cols-2 gap-6">
+                                <Input type="firstname" placeholder="Firstname" />
+                                <Input type="lastname" placeholder="Lastname" />
+                                <Input type="email" placeholder="E-mail address" />
+                                <Input type="phonenumber" placeholder="Phone number" />
+                            </div>
+                            <Select>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Select a service" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectLabel>Select a service</SelectLabel>
+                                        <SelectItem value="dev">Web Development</SelectItem>
+                                        <SelectItem value="gen">Genetics</SelectItem>
+
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                            <Textarea className="h-[200px]" placeholder="Write your message here" />
+                            <Button size="md" className="max-w-40 self-center xl:self-end">Send message</Button>
+                        </form>
+                    </div>
+                    {/* Contact info */}
+                    <div className="flex flex-1 items-center xl:justify-center order-1 xl:order-none  mb-8 xl:mb-0">
+                        <ul className="flex flex-col gap-10">
+                            {info.map((item, index) => {
+                                return (
+                                    <li key={index} className="flex items-center gap-6">
+                                        <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                                            <div>{item.icon}</div>
+                                        </div>
+                                        <div className="flex-1">
+                                            <p className="text-white/60">{item.title}</p>
+                                            <h3 className="text-xl">{item.description}</h3>
+                                        </div>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </motion.section>
     )
 }
