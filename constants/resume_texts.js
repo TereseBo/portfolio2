a1c5896f56f82200d3b297dcd4476d381f83d188
TereseBo <terese.boderus@gmail.com>
2024-12-13 12:49:20 +0100

    Updated resume texts

--- constants/resume_texts.js
+++ constants/resume_texts.js
@@ -3,22 +3,24 @@
 //TODO: Update educational data 
 //TODO: Update skill data 
 
-export const personal_desc=`Lorem ipsum odor amet, consectetuer adipiscing elit. Ornare cursus dui nullam pretium, 
-mauris tellus parturient? Rhoncus suscipit platea suscipit eleifend turpis facilisis mollis. Fringilla urna varius nisi 
-amet litora tellus at interdum nisi. Sed sodales turpis cras purus tellus consequat porttitor. Nascetur a condimentum ex 
-parturient rhoncus lobortis enim.`
+export const personal_desc=`I am passionate about problem-solving and thrive in environments where I 
+can apply my analytical skills to complex challenges. My background in genetics combined with my skills in web development 
+allows me to approach problems from both a scientific and technical perspective.
+I strive to deliver innovative solutions in the intersection of healthcare and technology.`
 
-export const experience_desc=`Lorem ipsum odor amet, consectetuer adipiscing elit. Ornare cursus dui nullam pretium, 
-mauris tellus parturient? Rhoncus suscipit platea suscipit eleifend turpis facilisis mollis. Fringilla urna varius nisi 
-amet litora tellus at interdum nisi. Sed sodales turpis cras purus tellus consequat porttitor. Nascetur a condimentum ex 
-parturient rhoncus lobortis enim.`
+export const experience_desc=`After many years working in genetics, including prenatal diagnostics and chromosomal analysis, 
+I shifted focus to programming to enhance my skills and improve workflow efficiency. In my current role as an Information Scientist at the Centre for Rare Diseases,
+ I’ve developed Office scripts and a small application to streamline the creation of custom ORPHAcodes, improving data handling and accuracy. 
+ This experience has allowed me to grow my programming abilities while optimizing daily operations. 
+`
 
-export const education_desc=`Lorem ipsum odor amet, consectetuer adipiscing elit. Ornare cursus dui nullam pretium, 
-mauris tellus parturient? Rhoncus suscipit platea suscipit eleifend turpis facilisis mollis. Fringilla urna varius nisi 
-amet litora tellus at interdum nisi. Sed sodales turpis cras purus tellus consequat porttitor. Nascetur a condimentum ex 
-parturient rhoncus lobortis enim.`
+export const education_desc=`I am a licensed biomedical analyst with additional training in bioinformatics,
+ NGS analysis, and project management. Most recently, I completed a higher vocational education in full-stack 
+ web development, where I gained skills in JavaScript, React, Next.js, and TypeScript. During my 5-month internship, 
+ I further developed my programming skills, including Python, and contributed to the development of software for 
+ visualizing gene sequencing results.`
 
-export const skill_desc=`Lorem ipsum odor amet, consectetuer adipiscing elit. Ornare cursus dui nullam pretium, 
-mauris tellus parturient? Rhoncus suscipit platea suscipit eleifend turpis facilisis mollis. Fringilla urna varius nisi 
-amet litora tellus at interdum nisi. Sed sodales turpis cras purus tellus consequat porttitor. Nascetur a condimentum ex 
-parturient rhoncus lobortis enim.`
+export const skill_desc=`I have strong programming skills in several languages including JavaScript, 
+React, Next.js, TypeScript, and Python. During my internship, I applied these skills to real-world projects,
+ such as creating software to visualize genetic data, and I continuously seek to improve my expertise in both 
+ web development and data handling.`
