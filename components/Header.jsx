215870b0f9d4e53236746d9e5e719a9d61137645
TereseBo <terese.boderus@gmail.com>
2024-12-28 16:17:09 +0100

    Added contact link to landing

--- components/Header.jsx
+++ components/Header.jsx
@@ -8,8 +8,8 @@ export function Header() {
         <header className="py-8 xl:py-12 text-white">
             <div className="container mx-auto flex justify-between items-center">
                 <Link href="/">
-                    <h1 className="text-4xl font semibold">
-                        Terese Bodérus <span className="text-accent"></span>
+                    <h1 className="text-4xl font semibold hover:text-accent">
+                        Terese Bodérus
                     </h1>
                 </Link>
                 {/* Nav for desktop */}
--- components/Socials.jsx
+++ components/Socials.jsx
@@ -1,16 +1,17 @@
 import Link from "next/link"
-import { FaGithub, FaLinkedinIn } from "react-icons/fa"
+import { FaGithub, FaLinkedinIn, FaEnvelope } from "react-icons/fa"
 
 const socials = [
-    { icon: <FaGithub />, path: "https://github.com/TereseBo" },
-    { icon: <FaLinkedinIn />, path: "https://linkedin.com/in/terese-bodérus-90b9731b6" },
+    { icon: <FaGithub />, path: "https://github.com/TereseBo", target:"_blank" },
+    { icon: <FaLinkedinIn />, path: "https://linkedin.com/in/terese-bodérus-90b9731b6", target:"_blank" },
+    { icon: <FaEnvelope />, path: "/contact", target:"_self" }
 ]
 export function Socials({ containerStyles, iconStyles }) {
     return (
         <div className={containerStyles}>
             {socials.map((social, index) => {
                 return (
-                    <Link href={social.path} target="_blank" key={index} className={iconStyles}>
+                    <Link href={social.path} target={social.target} key={index} className={iconStyles}>
                         {social.icon}
                     </Link>
                 )
